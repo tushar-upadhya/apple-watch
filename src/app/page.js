9728d37abbccf002d6a5c7eda2e46ea5be4cf4d2
tushar-upadhya <tusharupadhyay691@gmail.com>
2023-12-05 14:12:31 +0530

    completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,29 @@
+"use client";
+
 import Clock from "@/components/Clock";
 import Strap from "@/components/strap/Strap";
 import UpwardStrap from "@/components/strap/UpwardStrap";
+import { useState } from "react";
 
 export default function Home() {
+  let time = new Date();
+  let [hour, setHour] = useState();
+  let [minute, setMinute] = useState();
+
+  const updateTime = () => {
+    time = new Date();
+    setHour(n(time.getHours()));
+    setMinute(n(time.getMinutes()));
+  };
+  function n(n) {
+    return n > 9 ? "" + n : "0" + n;
+  }
+  setInterval(updateTime, 1000);
   return (
     <>
       <div className="scale-100">
         <UpwardStrap />
-        <Clock />
+        <Clock hour={hour} minute={minute} />
         <Strap />
       </div>
     </>
